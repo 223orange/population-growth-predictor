80283f389525f078a055ab458cd4453a0f5cb4b3
iliyanaydenov <iliya.naidenov@rewe-digital.com>
2022-07-25 19:07:42 +0300

    Create project

---
+++ src/test/java/com/example/populationgrowthpredictor/PopulationGrowthPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.populationgrowthpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopulationGrowthPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
